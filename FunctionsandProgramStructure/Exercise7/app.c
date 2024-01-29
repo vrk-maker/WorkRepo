a5251f635b2c28b57382a311cac2af079f8b91bd
vanshit.kamdar <vanshit.kamdar@volansys.com>
2024-01-29 17:54:56 +0530

    exercise7 done

---
+++ FunctionsandProgramStructure/Exercise7/app.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include<stdio.h>
+
+#define MAXLEN 100
+
+void itoa(int n, char s[]);
+
+
+int main(void) {
+    int n;
+    char s[MAXLEN];
+
+    n = 123;
+
+    itoa(n, s);
+
+    printf("%s", s);
+   
+    return 0;
+}
+
+void itoa(int n, char s[]) {
+    static int i;
+
+    if (n / 10)
+        itoa(n / 10, s);
+    else {
+        i = 0;
+        if (n < 0)
+            s[i++] = '-';
+    }
+
+    s[i++] = abs(n) % 10 + '0';
+
+    s[i] = '\0';
+}
+
