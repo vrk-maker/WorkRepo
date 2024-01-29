0d491467c06028b3526668497d595ec52ee597a9
vanshit.kamdar <vanshit.kamdar@volansys.com>
2024-01-29 16:37:11 +0530

    exercise4 done

---
+++ FunctionsandProgramStructure/Exercise4/app.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+#define PI 3.14159265
+int main()
+{
+    double x=12.0,result,y=60.0,ans,val;
+    //pow
+    printf("Value 2 ^ 3 = %lf\n", pow(2, 3));
+    //The ex in mathematics is equal to exp(x)
+    result=exp(x);
+    printf("exponential value = %lf\n",result);
+    //sin cosine
+    val = PI / 180.0; 
+    ans = cos(y * val); 
+    printf("The cosine of %lf is %lf degrees\n", y, ans); 
+    ans = sin(y * val);
+    printf("The sine of %lf is %lf degrees\n", y, ans);
+}
+
