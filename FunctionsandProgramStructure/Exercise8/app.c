316815460c3d8119a7350985986760670bcc0890
vanshit.kamdar <vanshit.kamdar@volansys.com>
2024-02-05 17:28:36 +0530

    exercise8 done

--- FunctionsandProgramStructure/Exercise8/app.c
+++ FunctionsandProgramStructure/Exercise8/app.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 
-void reverse(char *str)
-{
-  if (*str)
-  {
-    reverse(str + 1);
-    printf("%c", *str);
-  }
-}
- 
 
-int main()
-{
-  char a[] = "hello world";
-  reverse(a);
-  return 0;
+void reverseStringRecursive(char* str, int start, int end) {
+    if (start >= end) return;
+    char temp = str[start];
+    str[start] = str[end];
+    str[end] = temp;
+    reverseStringRecursive(str, ++start, --end);
+}
+
+void reverseString(char* str) {
+    reverseStringRecursive(str, 0, strlen(str) - 1);
+}
+
+int main() {
+    char str[] = "Hello, world!";
+    printf("Original string: %s\n", str);
+    reverseString(str);
+    printf("Reversed string: %s\n", str);
+    return 0;
 }
 
